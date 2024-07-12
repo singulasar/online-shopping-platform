2cf4975a8f67819f146f7450dcf99c0d0459e800
Singulasar <samdemeulenaere@hotmail.com>
2024-07-12 03:08:28 +0200

    minor ui improvement

--- resources/js/Components/ProductCard.vue
+++ resources/js/Components/ProductCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div @click="redirectToProduct" class="border rounded-lg p-4 shadow-md">
+  <div @click="redirectToProduct" class="border rounded-lg p-4 shadow-md cursor-pointer hover:bg-gray-100">
       <img :src="product.image_url" :alt="product.title" class="w-full h-48 object-cover mb-4">
       <h2 class="text-xl font-bold mb-2">{{ product.title }}</h2>
       <p class="text-gray-600 mb-2">{{ formatPrice(product.price) }} USD</p>
